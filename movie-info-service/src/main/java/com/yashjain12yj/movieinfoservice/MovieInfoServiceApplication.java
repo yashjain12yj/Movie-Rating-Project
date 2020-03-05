092500847f89610e196097b77a8c65cebed3cb1b
Yash Jain <yashjain12yj@gmail.com>
2020-03-05 13:01:08 +0530

    Init 3 microservices

---
+++ movie-info-service/src/main/java/com/yashjain12yj/movieinfoservice/MovieInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yashjain12yj.movieinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoServiceApplication.class, args);
+	}
+
+}
